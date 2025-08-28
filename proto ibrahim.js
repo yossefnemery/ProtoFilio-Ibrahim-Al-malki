19f3993d56ac8d3397c5e98e31022a6db8ad76b9
yossef nemery <https://github.com/yossefnemery>
2025-08-28 03:45:49 +0300

    final update

--- proto ibrahim.js
+++ proto ibrahim.js
@@ -25,11 +25,19 @@ document.addEventListener("mousemove", (e) => {
   });
 
 
+const toggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
 
- const toggle = document.getElementById("menu-toggle");
-  const navLinks = document.getElementById("nav-links");
+// فتح/قفل المينو
+toggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
 
-  toggle.addEventListener("click", () => {
-    navLinks.classList.toggle("show");
+// قفل المينو بعد الضغط على أي لينك
+const links = document.querySelectorAll(".nav-links a");
+links.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("show");
   });
+});
 
