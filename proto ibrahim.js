aa19ee0409f22a8929945f3a1da210adc6257850
yossef nemery <https://github.com/yossefnemery>
2025-08-28 03:37:50 +0300

    final

--- proto ibrahim.js
+++ proto ibrahim.js
@@ -24,3 +24,12 @@ document.addEventListener("mousemove", (e) => {
       });
   });
 
+
+
+ const toggle = document.getElementById("menu-toggle");
+  const navLinks = document.getElementById("nav-links");
+
+  toggle.addEventListener("click", () => {
+    navLinks.classList.toggle("show");
+  });
+
